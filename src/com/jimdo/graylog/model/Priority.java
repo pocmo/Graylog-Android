774c6e62f15b31d2c788ac01b640ba0bf1d0fb16
Sebastian Kaspari <s.kaspari@googlemail.com>
2009-11-11 23:54:25 +0100

    Helper class for priority levels

---
+++ src/com/jimdo/graylog/model/Priority.java
@@ -0,0 +1,37 @@
+/*
+Graylog - Android Client
+
+Copyright 2009 Sebastian Kaspari
+
+This file is part of Graylog (Android Client).
+
+Graylog (Android Client) is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+Graylog (Android Client) is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Graylog (Android Client). If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.jimdo.graylog.model;
+
+/**
+ * Helper class for priority levels
+ * 
+ * @author Sebastian Kaspari <s.kaspari@googlemail.com>
+ */
+public class Priority {
+	public static final int EMERGENCY 	= 0;
+	public static final int ALERT 		= 1;
+	public static final int CRITICAL 	= 2;
+	public static final int ERROR 		= 3;
+	public static final int WARNING 	= 4;
+	public static final int NOTICE 		= 5;
+	public static final int INFO 		= 6;
+	public static final int DEBUG 		= 7;
+}
