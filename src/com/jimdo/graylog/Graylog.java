5dc4134aad44335ff1fbf84f44ff3a4f0437a7a0
Sebastian Kaspari <s.kaspari@googlemail.com>
2009-11-11 17:50:15 +0100

    initial

---
+++ src/com/jimdo/graylog/Graylog.java
@@ -0,0 +1,13 @@
+package com.jimdo.graylog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Graylog extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
