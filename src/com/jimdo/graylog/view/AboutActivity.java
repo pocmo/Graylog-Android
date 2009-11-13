492b5b24423e2ed6547eb16e15e20a3aae17c5d9
Sebastian Kaspari <s.kaspari@googlemail.com>
2009-11-13 20:11:35 +0100

    every successful app has an about screen ;)

---
+++ src/com/jimdo/graylog/view/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.jimdo.graylog.view;
+
+import com.jimdo.graylog.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AboutActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
