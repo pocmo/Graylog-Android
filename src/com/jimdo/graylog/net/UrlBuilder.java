c67af49ffb02dd9a4daed9b04183a5406d987d52
Sebastian Kaspari <s.kaspari@googlemail.com>
2009-11-16 22:07:06 +0100

    UrlBuilder: Added support for HTTP Auth via URL

--- src/com/jimdo/graylog/net/UrlBuilder.java
+++ src/com/jimdo/graylog/net/UrlBuilder.java
@@ -20,12 +20,18 @@ along with Graylog (Android Client). If not, see <http://www.gnu.org/licenses/>.
 */
 package com.jimdo.graylog.net;
 
+import java.net.URL;
+
+import android.util.Log;
+
 /**
  * Helper class for creating API Urls
  * 
  * @author Sebastian Kaspari <s.kaspari@googlemail.com>
  */
 public class UrlBuilder {
+	public static final String TAG = "Graylog/UrlBuilder";
+	
 	private String baseUrl;
 	
 	/**
@@ -34,6 +40,49 @@ public class UrlBuilder {
 	 * @param baseUrl The base Url to Graylog
 	 */
 	public UrlBuilder(String baseUrl) {
+		// Test
+		this(baseUrl, "foo", "bar");
+		
+		setBaseUrl(baseUrl);
+	}
+	
+	/**
+	 * Create a new UrlBuilder with HTTP Auth Support
+	 * 
+	 * @param baseUrl The base Url to Graylog
+	 * @param httpUser Username for HTTP Auth
+	 * @param httpPassword Password for HTTP Auth
+	 */
+	public UrlBuilder(String baseUrl, String httpUser, String httpPassword) {
+		try {
+			URL url = new URL(baseUrl);
+			
+			String protocol = url.getProtocol();
+			String host = url.getHost();
+			String path = url.getPath();
+			int port    = url.getPort();
+			
+			baseUrl = protocol + "://" + httpUser + ":" + httpPassword + "@" + host;
+			
+			if (port != -1) {
+				baseUrl += ":" + port;
+			}
+			
+			baseUrl += path;
+			
+		} catch(Exception e) {
+			Log.d(TAG, "Exception: " + e.getMessage());
+		}
+		
+		setBaseUrl(baseUrl);
+	}
+	
+	/**
+	 * Set Base URL to Graylog
+	 * 
+	 * @param baseUrl The URL to Graylog
+	 */
+	private void setBaseUrl(String baseUrl) {
 		if (baseUrl.length() > 0 && baseUrl.charAt(baseUrl.length() - 1) !=  '/') {
 			baseUrl = baseUrl + '/'; 
 		}
