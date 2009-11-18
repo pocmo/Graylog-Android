d43561464e701006f1efa87eb55a7ac1a4f33034
Sebastian Kaspari <s.kaspari@googlemail.com>
2009-11-18 23:45:22 +0100

    Added licence and comment header

--- src/com/jimdo/graylog/CategoryListAdapter.java
+++ src/com/jimdo/graylog/CategoryListAdapter.java
@@ -1,3 +1,23 @@
+/*
+Graylog - Android Client
+
+Copyright 2009 Sebastian Kaspari
+
+This file is part of Graylog (Android Client).
+
+Graylog (Android Client) is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+Graylog (Android Client) is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Graylog (Android Client). If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.jimdo.graylog;
 
 import java.util.ArrayList;
@@ -13,6 +33,11 @@ import com.jimdo.graylog.model.Category;
 import com.jimdo.graylog.net.FetchCategoriesTask;
 import com.jimdo.graylog.net.UrlBuilder;
 
+/**
+ * Adapter for the List in CategoriesActivity
+ * 
+ * @author Sebastian Kaspari <s.kaspari@googlemail.com>
+ */
 public class CategoryListAdapter extends BaseAdapter {
 	public final static String TAG = "Graylog/CategoryListAdapter";
 	
